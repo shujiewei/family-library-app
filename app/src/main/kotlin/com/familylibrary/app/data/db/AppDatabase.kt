ff7cb333a65552f14497d6d16d929d4913aeedcf
shujiewei <723211770@qq.com>
2026-08-03 15:32:32 +0800

    ci: update app/src/main/kotlin/com/familylibrary/app/data/db/AppDatabase.kt

---
+++ app/src/main/kotlin/com/familylibrary/app/data/db/AppDatabase.kt
@@ -0,0 +1,46 @@
+package com.familylibrary.app.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.familylibrary.app.data.dao.AppSettingsDao
+import com.familylibrary.app.data.dao.BookDao
+import com.familylibrary.app.data.dao.BookshelfDao
+import com.familylibrary.app.data.dao.FamilyMemberDao
+import com.familylibrary.app.data.dao.ReadingRecordDao
+import com.familylibrary.app.data.dao.ShelfRowDao
+import com.familylibrary.app.data.dao.WishlistDao
+import com.familylibrary.app.data.entity.AppSettings
+import com.familylibrary.app.data.entity.Book
+import com.familylibrary.app.data.entity.Bookshelf
+import com.familylibrary.app.data.entity.FamilyMember
+import com.familylibrary.app.data.entity.ReadingRecord
+import com.familylibrary.app.data.entity.ShelfRow
+import com.familylibrary.app.data.entity.WishlistItem
+
+@Database(
+    entities = [
+        Bookshelf::class,
+        ShelfRow::class,
+        Book::class,
+        FamilyMember::class,
+        ReadingRecord::class,
+        WishlistItem::class,
+        AppSettings::class,
+    ],
+    version = SCHEMA_VERSION,
+    exportSchema = true,
+)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun bookshelfDao(): BookshelfDao
+    abstract fun shelfRowDao(): ShelfRowDao
+    abstract fun bookDao(): BookDao
+    abstract fun familyMemberDao(): FamilyMemberDao
+    abstract fun readingRecordDao(): ReadingRecordDao
+    abstract fun wishlistDao(): WishlistDao
+    abstract fun appSettingsDao(): AppSettingsDao
+
+    companion object {
+        const val SCHEMA_VERSION = 5
+        const val DB_NAME = "family_library.db"
+    }
+}
