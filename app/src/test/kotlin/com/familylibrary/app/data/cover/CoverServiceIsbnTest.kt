a758c4480be51647112ae034a298dfc1f770c130
shujiewei <723211770@qq.com>
2026-08-03 15:33:52 +0800

    ci: update app/src/test/kotlin/com/familylibrary/app/data/cover/CoverServiceIsbnTest.kt

---
+++ app/src/test/kotlin/com/familylibrary/app/data/cover/CoverServiceIsbnTest.kt
@@ -0,0 +1,21 @@
+package com.familylibrary.app.data.cover
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class CoverServiceIsbnTest {
+
+    @Test
+    fun normalizeIsbn_stripsHyphens() {
+        assertEquals("9780123456789", CoverService.normalizeIsbn("978-0-123456-78-9"))
+    }
+
+    @Test
+    fun isValidIsbn_accepts10And13() {
+        assertTrue(CoverService.isValidIsbn("0306406152"))
+        assertTrue(CoverService.isValidIsbn("9780306406157"))
+        assertFalse(CoverService.isValidIsbn("123"))
+    }
+}
