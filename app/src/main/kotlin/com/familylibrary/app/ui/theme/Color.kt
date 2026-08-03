7eaaea390a84a24252aa018d06eb785666fdc72e
shujiewei <723211770@qq.com>
2026-08-03 15:33:25 +0800

    ci: update app/src/main/kotlin/com/familylibrary/app/ui/theme/Color.kt

---
+++ app/src/main/kotlin/com/familylibrary/app/ui/theme/Color.kt
@@ -0,0 +1,22 @@
+package com.familylibrary.app.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBrown = Color(0xFF6D4C41)
+val PrimaryBrownLight = Color(0xFF9C786C)
+val SecondaryGreen = Color(0xFF558B2F)
+val BackgroundCream = Color(0xFFFFF8E1)
+val SurfaceWhite = Color(0xFFFFFBF5)
+val OnSurfaceDark = Color(0xFF3E2723)
+val AccentOrange = Color(0xFFE65100)
+
+val MemberColors = listOf(
+    Color(0xFF5C6BC0),
+    Color(0xFF26A69A),
+    Color(0xFFEF5350),
+    Color(0xFFFFA726),
+    Color(0xFFAB47BC),
+    Color(0xFF42A5F5),
+    Color(0xFF66BB6A),
+    Color(0xFF8D6E63),
+)
