88bcf9b9228123dfb8294453a0acc6ae2dafdd04
shujiewei <723211770@qq.com>
2026-08-21 16:28:42 +0800

    fix: scan debounce + ISBN lookup timeout (v1.1.4)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.familylibrary.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 5
-        versionName = "1.1.3"
+        versionCode = 6
+        versionName = "1.1.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
