fe05e9ec4ad00299a6e753b1f46e42e0fd696ddf
shujiewei <723211770@qq.com>
2026-08-21 18:08:57 +0800

    ci: update app/src/test/kotlin/com/familylibrary/app/data/cover/BookMetadataParserTest.kt

--- app/src/test/kotlin/com/familylibrary/app/data/cover/BookMetadataParserTest.kt
+++ app/src/test/kotlin/com/familylibrary/app/data/cover/BookMetadataParserTest.kt
@@ -63,4 +63,61 @@ class BookMetadataParserTest {
         val json = """{"totalItems": 0, "items": []}"""
         assertNull(BookMetadataParser.parseGoogleBooks(json))
     }
+
+    @Test
+    fun parseOpenLibrarySearch_extractsTitleAndAuthor() {
+        val json = """
+            {
+              "numFound": 1,
+              "docs": [{
+                "title": "Hong lou meng",
+                "author_name": ["Cao xue qin"],
+                "number_of_pages_median": 771
+              }]
+            }
+        """.trimIndent()
+        val parsed = BookMetadataParser.parseOpenLibrarySearch(json)
+        requireNotNull(parsed)
+        assertEquals("Hong lou meng", parsed.title)
+        assertEquals("Cao xue qin", parsed.author)
+        assertEquals(771, parsed.pageCount)
+    }
+
+    @Test
+    fun parseDouban_extractsChineseFields() {
+        val html = """
+            <html>
+            <head>
+              <meta property="og:title" content="红楼梦（下） (豆瓣)">
+              <meta property="og:image" content="https://img3.doubanio.com/view/subject/l/public/s123.jpg">
+            </head>
+            <body>
+              <h1><span property="v:itemreviewed">红楼梦（下）</span></h1>
+              <div id="info">
+                <span class="pl"> 作者</span>: <a href="/search/曹雪芹">曹雪芹</a><br/>
+                <span class="pl">出版社</span>: <a href="/press/1">人民文学出版社</a><br/>
+                <span class="pl">页数</span>: 771<br/>
+                <span class="pl">ISBN</span>: 9787020002207<br/>
+              </div>
+            </body>
+            </html>
+        """.trimIndent()
+
+        val parsed = BookMetadataParser.parseDouban(html, "9787020002207")
+        requireNotNull(parsed)
+        assertEquals("红楼梦（下）", parsed.title)
+        assertEquals("曹雪芹", parsed.author)
+        assertEquals("人民文学出版社", parsed.publisher)
+        assertEquals(771, parsed.pageCount)
+        assertEquals(
+            "https://img3.doubanio.com/view/subject/l/public/s123.jpg",
+            BookMetadataParser.doubanCoverUrl(html),
+        )
+    }
+
+    @Test
+    fun parseDouban_notFound_returnsNull() {
+        val html = """<html><body>没有找到关于 9787020002207 的书目信息</body></html>"""
+        assertNull(BookMetadataParser.parseDouban(html, "9787020002207"))
+    }
 }
