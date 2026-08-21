3f2b2d186453210a5f7283459ad6689663cfddd1
shujiewei <723211770@qq.com>
2026-08-21 16:41:50 +0800

    ci: update app/build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.familylibrary.app"
         minSdk = 24
         targetSdk = 34
-        versionCode = 6
-        versionName = "1.1.4"
+        versionCode = 7
+        versionName = "1.1.5"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables { useSupportLibrary = true }
     }
